57e57e363a2615050f444b36c86f6f8760e21041
Fernando Arriondo <arriondovfernando@gmail.com>
2024-02-29 03:40:37 -0300

    add slug slugify: enlaceUrl field

---
+++ src/api/pelicula/content-types/pelicula/lifecycles.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+var slugify = require('slugify')
+
+
+module.exports = {
+    async beforeCreate(pelicula) {
+        if (pelicula.params.data.titulo) {
+            pelicula.params.data.enlaceUrl = slugify(pelicula.params.data.titulo, { lower: true });
+        }
+    },
+    async beforeUpdate(pelicula) {
+        if (pelicula.params.data.titulo) {
+            pelicula.params.data.enlaceUrl = slugify(pelicula.params.data.titulo, { lower: true });
+        }
+    }
+}
